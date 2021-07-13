a0580436eb99da6dc7326c00e52b3871e780523d
Louis Meadows <louis.meadows@gmail.com>
2021-07-13 11:02:24 -0700

    Add files via upload

--- src/mainapplication.hpp
+++ src/mainapplication.hpp
@@ -13,11 +13,17 @@ public:
     explicit MainApplication(QObject *parent = nullptr);
     QSerialPort controlPort;
     int returnCode;
+    // ........ Boson Package (Before Bit Stuffing) ................
+    unsigned char   aux_boson_package[768];   // Max package without bit stuffing 768
+    unsigned short  aux_boson_package_len  = 0 ;   // To storage real size of package
+    unsigned char sequence=0;               // This number increases in every SENT commnand.
+
     int initilizeCommandPort(QString cmdPort);
     int getSerialNumber();
     int writeData(QByteArray &data);
     int flatFieldCorrection();
     int getFPAtemperature();
+    unsigned short CalcBlockCRC16(unsigned int bufferlen, unsigned char *buffer);
 public slots:
     void readData();
 signals:
